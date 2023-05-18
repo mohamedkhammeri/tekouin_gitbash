dc96a48a6ab3fd6169daf5738e2b316c62a8145a
mohamedkhammeri <mohamed.khammeri@esprit.tn>
2023-05-18 16:57:24 +0100

    test IFS

---
+++ task_9.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+IFS=$':'
+while read -r line; do
+    USERNAME=($line[0])
+    GROUP_ID=($line[3])
+    HOME_DIRECTORY=($line[5])
+    COMMAND_SHEL=($line[6])
+    USER_ID_INFO=($line[4])
+    PASSWORD=($line[1]) | awk -F: '{print $2}')
+    
